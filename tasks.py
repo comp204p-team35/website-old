13211c0fca222cb3b1c4ddaed88937d98d3bc021
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2016-11-18 14:19:50 +0000

    Modified tasks

--- tasks.py
+++ tasks.py
@@ -4,6 +4,9 @@ from invoke import task
 @task
 def build_site(c, production=False):
     if production:
+        print('building production site...')
         c.run('jekyll build --config=_config.yml,_config_prod.yml')
     else:
+        print('building development site...')
         c.run('jekyll build')
+    print('done')
